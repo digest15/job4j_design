ad584701c13204ccd1bc3a041175fad9cdccb735
digest15 <digest15@yandex.ru>
2022-05-14 19:15:12 +0800

    #252490 save filtered log

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,9 +1,7 @@
 package ru.job4j.io;
 
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -13,16 +11,28 @@ public class LogFilter {
     public List<String> filter(String file) {
         List<String> result = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("logs.txt"))) {
-            result = reader.lines().filter(str -> str.matches(".* 404 \\b")).collect(Collectors.toList());
+            result = reader.lines().filter(str -> str.matches(".* 404 .*")).collect(Collectors.toList());
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
         return result;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            log.forEach(out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
         System.out.println(log);
+        save(log, "404.txt");
     }
 }
