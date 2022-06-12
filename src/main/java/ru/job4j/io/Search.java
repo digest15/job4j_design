6300990866594504d6f749e921e7dcc18f79f4e4
digest15 <digest15@yandex.ru>
2022-06-12 18:21:41 +0800

    #106929 list files by predicate

---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, p -> p.toFile().getName().endsWith(".log")).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static class SearchFiles extends SimpleFileVisitor<Path> {
+        private List<Path> paths;
+        private Predicate<Path> condition;
+
+        public SearchFiles(Predicate<Path> condition) {
+            this.condition = condition;
+            paths = new ArrayList<>();
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if (!attrs.isDirectory()) {
+                if (condition.test(file)) {
+                    paths.add(file);
+                }
+            }
+            return FileVisitResult.CONTINUE;
+        }
+
+        public List<Path> getPaths() {
+            return paths;
+        }
+    }
+}
