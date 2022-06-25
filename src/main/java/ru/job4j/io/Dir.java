41bf7e4b0268ef06c2c92fe1f18e0dce1817398b
digest15 <digest15@yandex.ru>
2022-06-25 15:47:28 +0800

    Validate parameters in Dir

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -3,10 +3,12 @@ package ru.job4j.io;
 import java.io.File;
 
 public class Dir {
-    private static final String FILE_PATH = "./data";
-
     public static void main(String[] args) {
-        File file = new File(FILE_PATH);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
