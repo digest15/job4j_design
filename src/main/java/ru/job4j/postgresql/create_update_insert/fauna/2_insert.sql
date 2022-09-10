280fff330fda4008f0982257fcec5d7fbeb4b740
digest15 <digest15@yandex.ru>
2022-09-10 14:44:20 +0800

    #6861 select section

---
+++ src/main/java/ru/job4j/postgresql/create_update_insert/fauna/2_insert.sql
@@ -0,0 +1,3 @@
+insert into fauna (name, avg_age) values ('tiger', 7000);
+insert into fauna (name, avg_age, discovery_date) values ('superfish', 18000, '2022-10-09');
+insert into fauna (name, avg_age, discovery_date) values ('plainfish', 300, '1850-01-01');
