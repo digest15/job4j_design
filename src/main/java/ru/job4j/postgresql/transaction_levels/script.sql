090a1670dffbb7c267168de658d5e59a6ed8a46c
digest15 <digest15@yandex.ru>
2022-09-18 23:43:12 +0800

    #504813 cursor

--- src/main/java/ru/job4j/postgresql/transaction_levels/script.sql
+++ src/main/java/ru/job4j/postgresql/transaction_levels/script.sql
@@ -28,5 +28,35 @@ begin;
 commit;
 select * from products;
 
+--cursor
+insert into products (name, producer, count, price) values ('product_1', 'produser_1', 15, 100);
+insert into products (name, producer, count, price) values ('product_2', 'produser_2', 20, 200);
+insert into products (name, producer, count, price) values ('product_3', 'produser_3', 15, 100);
+insert into products (name, producer, count, price) values ('product_4', 'produser_4', 15, 100);
+insert into products (name, producer, count, price) values ('product_5', 'produser_5', 20, 200);
+insert into products (name, producer, count, price) values ('product_6', 'produser_6', 20, 200);
+insert into products (name, producer, count, price) values ('product_7', 'produser_7', 20, 200);
+insert into products (name, producer, count, price) values ('product_8', 'produser_8', 20, 200);
+insert into products (name, producer, count, price) values ('product_9', 'produser_9', 20, 200);
+insert into products (name, producer, count, price) values ('product_10', 'produser_10', 20, 200);
+insert into products (name, producer, count, price) values ('product_11', 'produser_11', 20, 200);
+insert into products (name, producer, count, price) values ('product_12', 'produser_12', 20, 200);
+insert into products (name, producer, count, price) values ('product_13', 'produser_13', 20, 200);
+insert into products (name, producer, count, price) values ('product_14', 'produser_14', 20, 200);
+insert into products (name, producer, count, price) values ('product_15', 'produser_15', 20, 200);
+insert into products (name, producer, count, price) values ('product_16', 'produser_16', 20, 200);
+insert into products (name, producer, count, price) values ('product_17', 'produser_17', 20, 200);
+insert into products (name, producer, count, price) values ('product_18', 'produser_18', 20, 200);
+insert into products (name, producer, count, price) values ('product_19', 'produser_19', 20, 200);
+insert into products (name, producer, count, price) values ('product_20', 'produser_10', 20, 200);
+
+begin;
+    declare cursor_products cursor for select * from products order by id;
+    fetch last from cursor_products;
+    fetch backward from cursor_products;
+    close cursor_products;
+commit;
+
+
 
 
