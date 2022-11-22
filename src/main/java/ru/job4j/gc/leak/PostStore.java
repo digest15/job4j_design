9508a84adbca228c06e6c19f61adfbbe1ed0387f
digest15 <digest15@yandex.ru>
2022-11-22 19:28:51 +0800

    #504882 #363054 int and Map

--- src/main/java/ru/job4j/gc/leak/PostStore.java
+++ src/main/java/ru/job4j/gc/leak/PostStore.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
-    private static HashMap<Integer, Post> posts = new HashMap<>();
+    private HashMap<Integer, Post> posts = new HashMap<>();
 
     private AtomicInteger atomicInteger = new AtomicInteger(1);
 
@@ -23,7 +23,7 @@ public class PostStore {
         posts.clear();
     }
 
-    public static Collection<Post> getPosts() {
+    public Collection<Post> getPosts() {
         return posts.values();
     }
 }
