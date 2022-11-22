6c153fdb710ba3814a513fecd218c7002bdf9fd7
digest15 <digest15@yandex.ru>
2022-11-22 13:29:31 +0800

    #504882 Implementing with leaks

---
+++ src/main/java/ru/job4j/gc/leak/PostStore.java
@@ -0,0 +1,29 @@
+package ru.job4j.gc.leak;
+
+import ru.job4j.gc.leak.model.Post;
+
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PostStore {
+    private static HashMap<Integer, Post> posts = new HashMap<>();
+
+    private AtomicInteger atomicInteger = new AtomicInteger(1);
+
+    public Post add(Post post) {
+        Integer id = atomicInteger.getAndIncrement();
+        post.setId(id);
+        posts.put(id, post);
+        return post;
+    }
+
+    public void removeAll() {
+        posts.clear();
+    }
+
+    public static Collection<Post> getPosts() {
+        return posts.values();
+    }
+}
