1deb93471631738ce7d48291406daf4325af979f
digest15 <digest15@yandex.ru>
2022-11-26 12:13:57 +0800

    #1592 Init code of task

---
+++ src/main/java/ru/job4j/gc/references/cache/AbstractCache.java
@@ -0,0 +1,19 @@
+package ru.job4j.gc.references.cache;
+
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractCache<K, V> {
+    protected final Map<K, SoftReference<V>> cache = new HashMap<>();
+
+    public void put(K key, V value) {
+
+    }
+
+    public V get(K key) {
+        return null;
+    }
+
+    protected abstract V load(K key);
+}
---
+++ src/main/java/ru/job4j/gc/references/cache/DirFileCache.java
@@ -0,0 +1,14 @@
+package ru.job4j.gc.references.cache;
+
+public class DirFileCache extends AbstractCache<String, String> {
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    protected String load(String key) {
+        return null;
+    }
+}
