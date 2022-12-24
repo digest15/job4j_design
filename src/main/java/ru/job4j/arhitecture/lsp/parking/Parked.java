eae4e4bda737b96696d3a772a4c8e89ab92de731
digest15 <digest15@yandex.ru>
2022-12-24 18:05:27 +0800

    #853
    Step 1. Create interfaces

---
+++ src/main/java/ru/job4j/arhitecture/lsp/parking/Parked.java
@@ -0,0 +1,5 @@
+package ru.job4j.arhitecture.lsp.parking;
+
+public interface Parked {
+    int parkSize();
+}
---
+++ src/main/java/ru/job4j/arhitecture/lsp/parking/Parking.java
@@ -0,0 +1,5 @@
+package ru.job4j.arhitecture.lsp.parking;
+
+public interface Parking {
+    boolean park(Parked auto);
+}
