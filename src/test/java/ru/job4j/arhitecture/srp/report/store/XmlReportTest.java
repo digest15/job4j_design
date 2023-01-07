c0ade4b78830a5008941b65e46828f1952055599
digest15 <digest15@yandex.ru>
2023-01-07 12:12:26 +0800

    #504874 #376545

--- src/test/java/ru/job4j/arhitecture/srp/report/store/XmlReportTest.java
+++ src/test/java/ru/job4j/arhitecture/srp/report/store/XmlReportTest.java
@@ -6,7 +6,9 @@ import ru.job4j.arhitecture.srp.report.XmlReport;
 import ru.job4j.arhitecture.srp.report.model.Employee;
 
 import javax.xml.bind.JAXBException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 import java.util.TimeZone;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,21 +20,31 @@ class XmlReportTest {
         Calendar date = Calendar.getInstance();
         date.set(1987, Calendar.AUGUST, 22, 0, 0, 0);
         date.setTimeZone(TimeZone.getTimeZone("Europe/Moscow"));
+        SimpleDateFormat formatter = new SimpleDateFormat("dd:MM:yyyy HH:mm");
+        String dateString = formatter.format(date.getTime());
         Employee worker = new Employee("Ivan", date, date, 100);
         store.add(worker);
         Report engine = new XmlReport(store);
-        String expect = """
-                <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-                <employees>
-                    <employee>
-                        <name>Ivan</name>
-                        <hired>22:08:1987 05:00</hired>
-                        <fired>22:08:1987 05:00</fired>
-                        <salary>100.0</salary>
-                    </employee>
-                </employees>
-                """;
-        assertThat(engine.generate(em -> true)).isEqualTo(expect);
+        StringBuilder expect = new StringBuilder()
+                .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>")
+                .append(System.lineSeparator())
+                .append("<employees>")
+                .append(System.lineSeparator())
+                .append("    <employee>")
+                .append(System.lineSeparator())
+                .append(String.format("        <name>%s</name>", worker.getName()))
+                .append(System.lineSeparator())
+                .append(String.format("        <hired>%s</hired>", dateString))
+                .append(System.lineSeparator())
+                .append(String.format("        <fired>%s</fired>", dateString))
+                .append(System.lineSeparator())
+                .append(String.format("        <salary>%.1f</salary>", worker.getSalary()))
+                .append(System.lineSeparator())
+                .append("    </employee>")
+                .append(System.lineSeparator())
+                .append("</employees>")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
     }
 
 }
